d325b5d2edc8ab99454f8dc1a39d7559ce45099c
Marina Gatsuk <marinkag@ukr.net>
2015-12-22 16:07:32 +0200

    Task interface update.

--- src/main/java/com/miratech/training/Task.java
+++ src/main/java/com/miratech/training/Task.java
@@ -1,7 +1,39 @@
 package com.miratech.training;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by vsytnyk on 17.12.2015.
  */
 public interface Task {
+    Integer getTaskID();
+    void setTaskID(Integer taskID);
+    Status getStatus();
+    void setStatus(Status status);
+    Person getAssignee();
+    void setAssignee(Person person);
+    Type getType();
+    void setType(Type type);
+    Priority getPriority();
+    void setPriority(Priority priority);
+    String getDescription();
+    void setDescription(String description);
+    String getSummary();
+    void setSummary(String summary);
+    List<Comment> getComments();
+    void addComment(Comment comment);
+    Date getDeadline();
+    void setDeadline(Date deadline);
+    Date getCreteDate();
+    void setCreateDate(Date createDate);
+    Person getReporter();
+    void setReporter(Person person);
+//
+//    Integer getCompleteness();
+//    void setCompleteness(Integer completeness);
+//    void startTask();
+//    void stopTask();
+//    void closeTask();
+
 }
