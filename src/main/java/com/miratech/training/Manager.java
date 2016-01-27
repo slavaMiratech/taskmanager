fbebc7eb831a9eadd5fb43d6beae6cd5a81396fd
Maksim Pushkar <Maksim.Pushkar@miratechgroup.com>
2016-01-27 09:34:12 +0200

    First sight at the  Manager interface

--- src/main/java/com/miratech/training/Manager.java
+++ src/main/java/com/miratech/training/Manager.java
@@ -1,7 +1,26 @@
 package com.miratech.training;
 
+import java.util.List;
+
 /**
  * Created by vsytnyk on 17.12.2015.
  */
 public interface Manager {
+    void removeTask(String name);
+    void removeAllTasks();
+    Task assignTask(String name);
+    List<Task> getTaskList();
+
+    void hireEmployee(Employee emp);
+    void fireEmployee(Employee emp);
+    List<Employee> getEmployeeList();
+
+    void assignTaskToEmployee(Task task, Employee emp);
+    void removeTaskFromEmployee(Task task, Employee emp);
+
+    boolean isEmployeeFree(Employee emp);
+    Integer getTaskStatus(Task task);
+
+    void waitTaskComplete(Task task);
+    void waitEmployee(Employee emp);
 }
