ec24423798bdc8036a9bfe7bb6efaedd3de844d0
Alena Katunina <Alena.Katunina@miratechgroup.com>
2016-01-28 14:34:03 +0200

    Task interface updated

--- src/main/java/com/miratech/training/Task.java
+++ src/main/java/com/miratech/training/Task.java
@@ -1,39 +1,29 @@
 package com.miratech.training;
 
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created by vsytnyk on 17.12.2015.
  */
 public interface Task {
-    Integer getTaskID();
-    void setTaskID(Integer taskID);
+    String getTaskID();
+    void setTaskID(String taskID);
     Status getStatus();
     void setStatus(Status status);
-    Person getAssignee();
-    void setAssignee(Person person);
-    Type getType();
-    void setType(Type type);
+    Employee getAssignee();
+    void setAssignee(Employee assignee);
     Priority getPriority();
     void setPriority(Priority priority);
     String getDescription();
     void setDescription(String description);
     String getSummary();
     void setSummary(String summary);
-    List<Comment> getComments();
-    void addComment(Comment comment);
-    Date getDeadline();
-    void setDeadline(Date deadline);
-    Date getCreteDate();
-    void setCreateDate(Date createDate);
-    Person getReporter();
-    void setReporter(Person person);
-//
-//    Integer getCompleteness();
-//    void setCompleteness(Integer completeness);
-//    void startTask();
-//    void stopTask();
-//    void closeTask();
-
+    Date getEndDate();
+    void setEndDate(Date endDate);
+    void addProgress(Integer progress);
+    Integer getProgress();
+//    Queue<Comment> getComments();
+//    void addComment(Comment comment);
+//    Manager getReporter();
+//    void setReporter(Manager reporter);
 }
