01d2ebc53101254e17e8f7406b86a7c747fa6b90
Ajene4ka <Ajene4ka@gmail.com>
2016-02-09 11:58:42 +0200

    Add employee

---
+++ src/main/java/com/miratech/training/EmployeeStatus.java
@@ -0,0 +1,13 @@
+package com.miratech.training;
+
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by eabramov on 17.12.2015.
+ */
+public interface EmployeeStatus {
+    final static  int FREE = 1;
+    final static  int BUSY = 2;
+    final static  int VACATION = 3;
+}
