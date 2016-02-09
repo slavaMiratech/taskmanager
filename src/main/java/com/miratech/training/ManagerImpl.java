b9da93a18ef48a17552af9fbd6a5ca012680ee26
Maksim Pushkar <Maksim.Pushkar@miratechgroup.com>
2016-02-09 11:50:45 +0200

    Second sight at Manager

---
+++ src/main/java/com/miratech/training/ManagerImpl.java
@@ -0,0 +1,120 @@
+package com.miratech.training;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by MaksimPs on 28.01.2016.
+ */
+public class ManagerImpl implements Manager {
+    private Logger log = LoggerFactory.getLogger(ManagerImpl.class);
+
+    List<Task> tasks = new LinkedList<>();
+    List<Employee> emps = new LinkedList<>();
+
+    static int maxId = 0;
+
+    Integer id;
+    String name;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ManagerImpl() {
+        maxId++;
+        this.id = maxId;
+        this.name = "Manager " + this.id;
+        log.info(name + " has created");
+    }
+
+    public ManagerImpl(String name) {
+        this();
+        this.name = name;
+    }
+
+    // добавить задачу менеджеру
+    public void assignTask(Task task){
+        log.info("Assign task \"" + task.getDescription() + "\"");
+        tasks.add(task);
+    }
+
+    // удалить задачу из списка задач
+    public void removeTask(Task task) {
+        log.info("Remove task \"" + task.getDescription() + "\"");
+        tasks.remove(task);
+    }
+
+    // удалить все задачи
+    public void removeAllTasks() {
+        log.info("Removing all tasks");
+        tasks.clear();
+        log.info("All tasks have  removed");
+    }
+
+    // получить все задачи
+    public List<Task> getTaskList(){
+        return tasks;
+    }
+
+    // нанять сотрудника в проект
+    public void hireEmployee(Employee emp){
+        log.info("Add " + emp);
+        emps.add(emp);
+    }
+
+    // убрать сотрудника из проекта
+    public void fireEmployee(Employee emp){
+        log.info("Remove " + emp);
+        emps.remove(emp);
+    }
+
+    // список сотрудников в проекте
+    public List<Employee> getEmployeeList(){
+        return emps;
+    }
+
+    // назначить задачу сотруднику
+    private void assignTaskToEmployee(Task task, Employee emp){
+//        emp.assignTask(task);
+    }
+
+    // забрать задачу у сотрудника
+    private void removeTaskFromEmployee(Task task, Employee emp){
+//        emp.removeTask(task);
+    }
+
+    // свободен ли сотрудник
+    private boolean isEmployeeFree(Employee emp){
+//        return emp.IsFree();
+        return false;
+    }
+
+    // получить статус задачи
+    private Integer getTaskStatus(Task task){
+//        return task.getStatus();
+        return -1;
+    }
+
+    // ожидать завершения задачи
+    public void waitTaskComplete(Task task){
+//        if (task.getStatus() != Status.CLOSED) {
+//            task.wait();
+//        }
+    }
+
+    // ожидать свободен ли сотрудник
+    public void waitEmployee(Employee emp){
+//        if (!emp.isFree()) {
+//            emp.wait();
+//        }
+    }
+}
---
+++ src/test/java/com/miratech/training/ManagerImplTest.java
@@ -0,0 +1,44 @@
+package com.miratech.training;
+
+import com.miratech.service.SayService;
+import com.miratech.service.SayServiceImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * Created by MaksimPs on 28.01.2016.
+ */
+public class ManagerImplTest {
+
+    String defName = "ManagerTest";
+    Manager manager = new ManagerImpl(defName);
+
+    @Test
+    public void test() throws Exception {
+        String name = manager.getName();
+        Assert.assertNotNull(name);
+        Assert.assertTrue(name.equals(defName));
+    }
+
+    @Test
+    public void testAddRemoveTask() throws Exception {
+        Task task = new TaskImpl();//("task1", "summ", "descr", Priority.MAJOR, 10, new Date());
+        Assert.assertNotNull(task);
+        manager.assignTask(task);
+        Assert.assertTrue(manager.getTaskList().contains(task));
+        manager.removeTask(task);
+        Assert.assertFalse(manager.getTaskList().contains(task));
+    }
+
+    @Test
+    public void testAddRemoveEmployee() throws Exception {
+        Employee emp = new Employee() {};
+        Assert.assertNotNull(emp);
+        manager.hireEmployee(emp);
+        Assert.assertTrue(manager.getEmployeeList().contains(emp));
+        manager.fireEmployee(emp);
+        Assert.assertFalse(manager.getEmployeeList().contains(emp));
+    }
+}
