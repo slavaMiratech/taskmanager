a17de14e6e3be06667e2c4a1bb73eb88e3332457
ostapMiratech <ostap.bilyk@i.ua>
2016-02-09 11:45:23 +0200

    remove task of group2

--- src/main/java/com/miratech/training/Task.java
+++ src/main/java/com/miratech/training/Task.java
@@ -1,29 +1,40 @@
 package com.miratech.training;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.Collection;
 
 /**
  * Created by vsytnyk on 17.12.2015.
  */
 public interface Task {
-    String getTaskID();
-    void setTaskID(String taskID);
-    Status getStatus();
-    void setStatus(Status status);
-    Employee getAssignee();
-    void setAssignee(Employee assignee);
-    Priority getPriority();
-    void setPriority(Priority priority);
+    void startWorkOnTask();
+    void stopWorkOnTask();
+    void completeTask();
+    void setStatus(TaskStatus newStatus);
+    TaskStatus getStatus();
+
+    void setPriority(TaskPriority priority);
+    TaskPriority getPriority();
+    void sendReminder();
+    void sendNotification();
+    void addComment(TaskComment comment);
+    void removeComment(TaskComment commment);
+    Collection<TaskComment> getComments();
+    void addEmployees(Collection<Employee> employee);
+    Collection<Employee> getEmployees();
+    void removeEmployees(Collection<Employee> employee);
+    void calculateRemainingTime();
+
     String getDescription();
-    void setDescription(String description);
-    String getSummary();
-    void setSummary(String summary);
-    Date getEndDate();
-    void setEndDate(Date endDate);
-    void addProgress(Integer progress);
-    Integer getProgress();
-//    Queue<Comment> getComments();
-//    void addComment(Comment comment);
-//    Manager getReporter();
-//    void setReporter(Manager reporter);
+    void setDescription(String newDescrioption);
+
+    void setSupervisor(Manager manager);
+    Manager getSupervisor();
+
+    void setEstimate(long seconds);
+    long getEstimate();
+    void setDeadline(Calendar date);
+    Calendar getDeadline();
+    void setStartDate(Calendar startDate);
+    Calendar getStartDate();
 }
